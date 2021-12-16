604b478f7820c0f7f037bb036f3a3fb55dabbc5d
Isaias <isaias_abel.23@outlook.com>
2021-12-15 19:57:35 -0500

    vistas 6.0

--- App/Views/Home/newelcome.php
+++ App/Views/Home/newelcome.php
@@ -12,3 +12,15 @@
   <a class="btn btn-secondary" href="?controller=Register&&action=index" role="button">Registrarse</a>
 
   </section> 
+  <section id="A" style="border-radius: 100px 100px 100px 100px; width: 100%;  height: 100%; text-align: center; color: #000000 ; float: left; background: #BAFDFF; margin-top: 0%;">
+
+</p></p><h1> Bienvenido a SIGMED </p></p></h1>
+<p> Aquí podra gestionar y agendar sus citas meédicas <br>para su atención en alguna de las policlínicas <br> de la Caja de Seguro Social </p>
+ <p> La salud es la mayor posesión. La alegría es el mayor tesoro.</p>
+  <p><a class="btn btn-success" href="?controller=Login&&action=index" role="button">Iniciar Sesión</a> o 
+  <a class="btn btn-secondary" href="?controller=Register&&action=index" role="button">Registrarse</a>
+  <div class="container" style="height: 10%;">
+  <img class="img-fluid" src="imagen\css.png" alt="sigmed" width="20%" height="10%">
+
+    </div>
+</main>
