c78acf8f41fef0479bfc15f82b64b88092d3da12
Isaias <isaias_abel.23@outlook.com>
2021-12-15 21:25:20 -0500

    vistas 7.0

--- App/Views/Home/welcome.php
+++ App/Views/Home/welcome.php
@@ -1,12 +1,14 @@
 <section id="A" style="border-radius: 0px 35px 35px 0px; width: 40%;  height: 100%; text-align: center; color: #002973 ; float: left; background: #BAFDFF; margin-top: 0%;">
-<div class="container" style="height: 10%;">
-  <img class="img-fluid" src="imagen\css2.png" alt="sigmed" width="500" height="450">
+<div class="container" style="height: 10%; margin-top: 5%">
+  <img class="img-fluid" src="imagen\css1.png" alt="sigmed" width="500" height="450">
 </div>
   </section> 
 
-  <section id="B" style="padding: 3% 0% 3% 0%; width: 60%; height: 10%; text-align: center; float: right; text-overflow: inherit;">
-  </p></p><h1> BIENVENIDO A SIGMED </h1></h1>
-  <p> Aquí podra gestionar y agendar sus citas meédicas <br>para su atención en alguna de las policlínicas <br> de la Caja de Seguro Social </p>
+  <section id="B" style="padding: 3% 0% 3% 0%; width: 60%; height: 100%; text-align: center; float: right; text-overflow: inherit;">
+  <br>
+  <br><br>  
+</p></p><h1> BIENVENIDO A SIGMED </h1></h1>
+  <p> Aquí podrá gestionar y agendar sus citas médicas <br>para su atención en alguna de las policlínicas <br> de la Caja de Seguro Social </p>
  <p> La salud es la mayor posesión. La alegría es el mayor tesoro.</p>
   <p><a class="btn btn-success" href="?controller=Login&&action=index" role="button">Iniciar Sesión</a> o 
   <a class="btn btn-secondary" href="?controller=Register&&action=index" role="button">Registrarse</a>
